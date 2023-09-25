8461f0bba18505bfc868ceb74b50b08021d1ef66
IgorTest001 <frolov@bsuir.by>
2023-09-25 21:29:16 +0300

    Breadcrumbs and Toastr-messages were added.

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IProduct } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -11,7 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
 
-  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute) { }
+  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute, private breadCrumbService: BreadcrumbService) { }
 
   ngOnInit() {
     this.loadProduct();
@@ -20,6 +21,7 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct() {
     this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(product => {
       this.product = product;
+      this.breadCrumbService.set('@productDetails', product.name); // to get and display the entity-product name in the productDetailsComponent-view-breadcrumb for each of the products
     })
   }
 
