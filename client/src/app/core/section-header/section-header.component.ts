7c6f8fcf697a2135fb62a260184d282f0d81827c
IgorTest001 <frolov@bsuir.by>
2023-11-02 19:29:38 +0300

    Breadscrumbs were added.

--- client/src/app/core/section-header/section-header.component.ts
+++ client/src/app/core/section-header/section-header.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-section-header',
   templateUrl: './section-header.component.html',
   styleUrls: ['./section-header.component.scss']
 })
-export class SectionHeaderComponent {
+export class SectionHeaderComponent implements OnInit{
+  breadscrumb$: Observable<any[]>;
+  constructor(private breadscrumbService: BreadcrumbService) { }
+
+  ngOnInit(): void {
+    this.breadscrumb$ = this.breadscrumbService.breadcrumbs$;
+  }
 
 }
