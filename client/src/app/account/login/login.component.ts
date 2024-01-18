55d07aa0179e17164e8e97b4228943db58dce580
IgorTest001 <frolov@bsuir.by>
2024-01-18 19:08:10 +0300

    Login and resigester components were updated using text-imput-component, ActivatedRoute and Email-validators.

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AccountService } from '../account.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,10 +10,12 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit{
   loginForm: FormGroup;
+  returnUrl: string;
 
-  constructor(private accountService: AccountService, private router: Router) {}
+  constructor(private accountService: AccountService, private router: Router, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.returnUrl = this.activatedRoute.snapshot.queryParams.returnUrl || '/shop';
     this.createLoginForm();
   }
 
@@ -29,7 +31,8 @@ export class LoginComponent implements OnInit{
     this.accountService.login(this.loginForm.value).subscribe({
                                                       next: () => {
                                                         //console.log('User logged in.');
-                                                        this.router.navigateByUrl('/shop');
+                                                        //this.router.navigateByUrl('/shop');
+                                                        this.router.navigateByUrl(this.returnUrl);
                                                       },
                                                       error: error => {
                                                         console.log(error);
