051e6c0b7651e754fecaa07a379f7d9e16e7de2c
IgorTest001 <frolov@bsuir.by>
2024-02-17 17:13:44 +0300

    UnitOfWork added and list of injected services extended.

--- API/Extenstions/ApplicationExtensions.cs
+++ API/Extenstions/ApplicationExtensions.cs
@@ -1,5 +1,6 @@
 using API.Errors;
 using Core.Interfaces;
+using Infrastructure;
 using Infrastructure.Data;
 using Infrastructure.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -38,6 +39,8 @@ namespace API.Extenstions
             });
 
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddScoped<IBasketRepository, BasketRepository>();
