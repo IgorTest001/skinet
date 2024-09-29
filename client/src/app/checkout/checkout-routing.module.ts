04f37df2cacd00aaabfba43e4a1ad5ebe6e3ffff
IgorTest001 <frolov@bsuir.by>
2024-09-29 21:26:11 +0300

    Checkout-component with CdkStepper has been added.

--- client/src/app/checkout/checkout-routing.module.ts
+++ client/src/app/checkout/checkout-routing.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutComponent } from './checkout.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CheckoutSuccessComponent } from './checkout-success/checkout-success.component';
 
 const routes: Routes = [
-  {path: '', component: CheckoutComponent}
+  {path: '', component: CheckoutComponent},
+  {path: 'success', component: CheckoutSuccessComponent}
 ]
 
 @NgModule({
   declarations: [],
   imports: [
-    //CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [ RouterModule ]
 })
--- client/src/app/checkout/checkout.component.ts
+++ client/src/app/checkout/checkout.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgControl, FormControl, FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AccountService } from '../account/account.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
+  checkoutParentForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private accountService: AccountService) { }
+
+  ngOnInit() {
+    this.createCheckoutForm();
+    this.getAddressFormValues();
+  }
+
+  createCheckoutForm(){
+    this.checkoutParentForm = this.formBuilder.group({
+      addressForm: this.formBuilder.group({     // addressForm has the same name as addressForm has in the checkout-address.component.html
+        firstName: [null, Validators.required], // this addressForm serves as parent form, which serves as input for the child addressForm in checkout-address.component.html
+        lastName: [null, Validators.required],
+        street: [null, Validators.required],
+        city: [null, Validators.required],
+        state: [null, Validators.required],
+        zipcode: [null, Validators.required]
+      }),
+      deliveryForm: this.formBuilder.group({
+        deliveryMethod: [null, Validators.required]
+      }),
+      paymentForm: this.formBuilder.group({
+        nameOnCard: [null, Validators.required]
+      })
+    });
+  }
+
+  getAddressFormValues() {
+    this.accountService.getUserAddress().subscribe(address => {
+      if (address) {
+        this.checkoutParentForm.get('addressForm').patchValue(address);
+      }
+    }, error => {
+      console.log(error);
+    });
+  }
 
 }
