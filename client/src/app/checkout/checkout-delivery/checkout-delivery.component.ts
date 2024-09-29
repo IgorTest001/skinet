7acb426c8474d5d51d628bf35f2f22ad9c944d37
IgorTest001 <frolov@bsuir.by>
2024-09-29 21:43:41 +0300

    Checkout-delivery component and related things added.

---
+++ client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { CheckoutService } from '../checkout.service';
+import { IDeliveryMethod } from 'src/app/shared/models/deliveryMethod';
+import { BasketService } from 'src/app/basket/basket.service';
+
+@Component({
+  selector: 'app-checkout-delivery',
+  templateUrl: './checkout-delivery.component.html',
+  styleUrls: ['./checkout-delivery.component.scss']
+})
+export class CheckoutDeliveryComponent implements OnInit{
+  @Input() checkoutDeliveryForm: FormGroup;
+  deliveryMethods: IDeliveryMethod[];
+
+  constructor(private checkoutService: CheckoutService, private basketService: BasketService) {}
+
+  ngOnInit(): void {
+    this.checkoutService.getDeliveryMethods()
+                        .subscribe({
+                          next: (dm: IDeliveryMethod[]) => {
+                            this.deliveryMethods = dm;
+                            },
+                          error: error => {
+                            console.log(error);
+                          }
+                        });
+  }
+
+  setShippingPrice(deliveryMethod: IDeliveryMethod){
+    this.basketService.setShippingPrice(deliveryMethod);
+  }
+}
